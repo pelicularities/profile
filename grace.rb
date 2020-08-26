e25d64eded5d06fbdfaac59df66e1bc710a51ab0
Grace Teng <grace@pelicularities.net>
2020-08-26 16:42:22 +0800

    added more fake Ruby code to profile

--- grace.rb
+++ grace.rb
@@ -3,18 +3,42 @@ require 'singleton'
 # Grace
 class Grace
   include Singleton # there can only be one
-  attr_reader :likes, :education
-  def initialize
-    likes = [
-      "musicals",
-      "video games",
-      "technology"
+
+  def interests
+    ['musicals', 'video games', 'technology']
+  end
+
+  def jobs
+    [
+      {
+        company: 'Mednefits',
+        role: 'Customer Support Hero / QA Tester',
+        start: 2019,
+        end: 2020
+      },
+      {
+        company: 'The Learning Lab',
+        role: 'Specialist Teaching Associate',
+        start: 2015,
+        end: 2017
+      }
     ]
-    @education = {
-      university: "New York University",
-      graduated: 2014,
-      majors: ["Film and TV", "Spanish"],
-      minors: ["Linguistics"]
-    }
+  end
+
+  def education
+    [
+      {
+        school: 'Le Wagon',
+        campus: 'Singapore',
+        course: 'Full-Stack Web Development',
+        learning: ['Ruby', 'OOP', 'SQL', 'HTML', 'CSS']
+      },
+      {
+        school: 'New York University',
+        graduated: 2014,
+        majors: ['Film and TV', 'Spanish'],
+        minors: ['Linguistics']
+      }
+    ]
   end
 end
